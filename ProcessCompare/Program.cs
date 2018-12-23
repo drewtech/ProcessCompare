43bbc4781d2ebca20614d3f7a6a693c7c07a70ca
Andrew Widdowson <drewtechau@gmail.com>
2018-12-23 15:31:28 +1100

    Initial commit

---
+++ ProcessCompare/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+using Serilog;
+
+namespace ProcessCompare
+{
+    class Program
+    {
+        private static Settings _settings;
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("ProcessCompare.log")
+                .MinimumLevel.Verbose()
+                .CreateLogger();
+
+            Log.Information("Starting ProcessCompare {args}", args);
+
+            _settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "settings.json")));
+            var comparer = new Comparer(_settings);
+            comparer.DoCompare();
+
+            Log.Information("Closing Process Compare.");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ ProcessCompare/Settings.cs
@@ -0,0 +1,11 @@
+﻿namespace ProcessCompare
+{
+    internal class Settings
+    {
+        public string CompareExportFolder { get; set; }
+        public string CompareProjectLocation { get; set; }
+        public string CompareProjectName { get; set; }
+        public string RedGateDataComparePath { get; set; }
+
+    }
+}
