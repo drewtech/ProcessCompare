f296302ff69bf80d4dfb3a40d3ac6c1d20f5c986
Andrew Widdowson <drewtechau@gmail.com>
2019-01-06 10:35:13 +1100

    Parse Compare report to get columns sizes and create tables

--- ProcessCompare/MigrationScriptGenerator.cs
+++ ProcessCompare/MigrationScriptGenerator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -21,6 +22,12 @@ namespace ProcessCompare
         {
             Log.Information("Generating migration scripts from csv export.");
 
+            Log.Warning("Deleting any existing migration scripts...");
+
+            DeleteExistingMigrationScripts();
+
+            Log.Warning("Migration scripts deleted.");
+
             foreach (var file in GetFileList())
             {
                 GenerateCreateTableScript(file);
@@ -40,28 +47,54 @@ namespace ProcessCompare
             var sb = new StringBuilder();
             var tableName = Path.GetFileNameWithoutExtension(file.Name);
 
-            sb.AppendLine($"CREATE TABLE {tableName}( ");
+            var columnSizeMap = MapColumnSize(file.FullName);
 
-            using (var reader = new StreamReader(file.FullName))
+            sb.AppendLine($"CREATE TABLE {tableName}( ");
+            var tableKeys = columnSizeMap.Keys.Where(x => x.EndsWith("_k"));
+
+            foreach (var kvp in columnSizeMap)
+            {
+                sb.AppendLine($"[{kvp.Key}] varchar({kvp.Value}),");
+            }
+            sb.AppendLine($"PRIMARY KEY ({string.Join(',', tableKeys)})");
+            sb.AppendLine(");");
+
+            File.AppendAllText(Path.Combine(_settings.MigrationScriptLocation, $"{tableName}.sql"), sb.ToString());
+        }
+
+        private Dictionary<string, int> MapColumnSize(string fullName)
+        {
+            var firstRow = true;
+            var columnSizeMap = new Dictionary<string, int>();
+            using (var reader = new StreamReader(fullName))
             {
                 using (var csv = new CsvReader(reader))
                 {
-                    csv.Read();
-                    csv.ReadHeader();
-                    string[] headerRow = csv.Context.HeaderRecord;
-                    var keys = GetKeys(headerRow);
-                    foreach (var row in headerRow)
+                    while (csv.Read())
                     {
-                        //TODO don't use varchar(max) for everything
-                        sb.AppendLine($"[{row}] varchar(max),");
-                    }
+                        if (firstRow)
+                        {
+                            csv.ReadHeader();
+                            firstRow = false;
+                            continue;
+                        }
 
-                    sb.AppendLine($"PRIMARY KEY ({string.Join(',', keys)})");
-                    sb.AppendLine(");");
+                        foreach (var header in csv.Context.HeaderRecord)
+                        {
+                            if (!columnSizeMap.ContainsKey(header))
+                                columnSizeMap.Add(header, csv.GetField<string>(header).Length);
+                            else
+                            {
+                                if (csv.GetField<string>(header).Length > columnSizeMap[header])
+                                    columnSizeMap[header] = csv.GetField<string>(header).Length;
+                            }
+                            
+                        }
+                    }
                 }
             }
 
-            File.AppendAllText(Path.Combine(_settings.MigrationScriptLocation, $"{tableName}.sql"), sb.ToString());
+            return columnSizeMap;
         }
 
         private IEnumerable<string> GetNormalRows(string[] headerRow)
@@ -69,9 +102,13 @@ namespace ProcessCompare
             return headerRow.Where(x => !x.EndsWith("_k"));
         }
 
-        private IEnumerable<string> GetKeys(string[] headerRow)
+        private void DeleteExistingMigrationScripts()
         {
-            return headerRow.Where(x => x.EndsWith("_k"));
+            var di = new DirectoryInfo(_settings.MigrationScriptLocation);
+            foreach (FileInfo file in di.GetFiles("*.sql"))
+            {
+                file.Delete();
+            }
         }
 
         private IList<FileInfo> GetFileList()
